a003bd37d43b1691da24335e7232037305116390
= <=>
2016-03-27 17:14:42 -0600

    Efecto de zoom

--- Proyecto/app/src/main/java/com/example/alan32/proyecto/Inicio.java
+++ Proyecto/app/src/main/java/com/example/alan32/proyecto/Inicio.java
@@ -2,15 +2,17 @@ package com.example.alan32.proyecto;
 /*Importando  las librerias de andorid necesarias*/
 
 import android.app.*;
-import android.content.Intent;
 import android.os.*;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.*;
 
 public class Inicio extends Activity implements View.OnClickListener{
 
     private TextView mensaje;
     private Button iniciar;
+    ImageView imagen;
 
     public void onCreate(Bundle b){
         super.onCreate(b);
@@ -21,13 +23,23 @@ public class Inicio extends Activity implements View.OnClickListener{
     private void enlaza(){
         mensaje = (TextView)findViewById(R.id.xmensaje);
         iniciar = (Button)findViewById(R.id.xiniciar);
+        imagen = (ImageView)findViewById(R.id.ximagen);
         iniciar.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        Toast toat = Toast.makeText(getApplicationContext(),"Iniciando aplicación",Toast.LENGTH_LONG);
-        toat.show();
+        switch(v.getId()){
+            case R.id.xiniciar:   Toast toat = Toast.makeText(getApplicationContext(),"Iniciando aplicación",Toast.LENGTH_LONG);
+                                              toat.show();
+                                              break;
+            case R.id.ximagen:
+                                                Animation mizoom = AnimationUtils.loadAnimation(this,R.anim.acercar);
+                                                mizoom.reset();
+                                                imagen.startAnimation(mizoom);
+                                                break;
+        }
+
 
     }
 }
