bae1bea1c37240ff99d35ac531fe3f58b53177cb
= <=>
2016-03-27 22:01:25 -0600

    Casi terminado

--- Proyecto/app/src/main/java/com/example/alan32/proyecto/Inicio.java
+++ Proyecto/app/src/main/java/com/example/alan32/proyecto/Inicio.java
@@ -7,26 +7,41 @@ import android.os.*;
 import android.view.View;
 import android.widget.*;
 
-public class Inicio extends Activity implements View.OnClickListener{
+public class Inicio extends Activity implements View.OnClickListener {
 
     private TextView mensaje;
+    private TextView nombre;
+    private EditText cajanombre;
     private Button iniciar;
 
-    public void onCreate(Bundle b){
+    public void onCreate(Bundle b) {
         super.onCreate(b);
         setContentView(R.layout.activity_inicio);
         enlaza(); /*En este metodo  se enlazan los componentes con los xml*/
     }
 
-    private void enlaza(){
-        mensaje = (TextView)findViewById(R.id.xmensaje);
-        iniciar = (Button)findViewById(R.id.xiniciar);
+    private void enlaza() {
+        mensaje = (TextView) findViewById(R.id.xmensaje);
+        nombre = (TextView) findViewById(R.id.xnombre);
+        cajanombre = (EditText) findViewById(R.id.xcaja);
+        iniciar = (Button) findViewById(R.id.xiniciar);
         iniciar.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        Toast toat = Toast.makeText(getApplicationContext(),"Iniciando aplicación",Toast.LENGTH_LONG);
-        toat.show();
+        String nombre = cajanombre.getText().toString();
+            if (nombre.equals("")) {
+                Toast toat = Toast.makeText(getApplicationContext(), "Ingresa tu nombre", Toast.LENGTH_LONG);
+                toat.show();
+            } else {
+                Toast toat = Toast.makeText(getApplicationContext(), "Iniciando aplicación", Toast.LENGTH_LONG);
+                toat.show();
+                Intent nvo = new Intent(Inicio.this, Prince.class);
+                Bundle bun = new Bundle();
+                bun.putString("Nombre", cajanombre.getText().toString());
+                nvo.putExtras(bun);
+                startActivity(nvo);
+            }
     }
 }
--- Proyecto/app/src/main/java/com/example/alan32/proyecto/Prince.java
+++ Proyecto/app/src/main/java/com/example/alan32/proyecto/Prince.java
@@ -2,10 +2,13 @@ package com.example.alan32.proyecto;
 
 /*Importando  las librerias de andorid necesarias*/
 import android.app.*;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.*;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.*;
 
 public class Prince extends Activity implements View.OnClickListener{
@@ -22,28 +25,48 @@ public class Prince extends Activity implements View.OnClickListener{
 
     private void enlaza(){
         mensaje = (TextView)findViewById(R.id.xmensaje);
-        //cargar = (Button)findViewById(R.id.xcarga);
-        //imgview = (ImageView)findViewById(R.id.ximagen);
+        Bundle bun = this.getIntent().getExtras();
+        mensaje.append(" "+bun.getString("Nombre"));
+        cargar = (Button)findViewById(R.id.xcarga);
+        imgview = (ImageView)findViewById(R.id.ximagen);
+        imgview.setOnClickListener(this);
         cargar.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-            AlertDialog.Builder builder = new AlertDialog.Builder(Prince.this);
-            Intent in = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-            in.setType("image/*");
-            startActivityForResult(in.createChooser(in, "Selecciona una imagen"), 200);
-            builder.show();
-    }
+           switch(v.getId()){
+               case R.id.ximagen:       Animation anima = AnimationUtils.loadAnimation(this,R.anim.acercar);
+                                                       anima.reset();
+                                                       imgview.startAnimation(anima);
+                                                        break;
+               case R.id.xcarga:    final CharSequence[] opciones= {"Elige imagen","Cancelar"};
+                                                 AlertDialog.Builder builder = new AlertDialog.Builder(Prince.this);
+                                                builder.setTitle("Elige una opción");
+                                                builder.setItems(opciones, new DialogInterface.OnClickListener() {
+                                                    @Override
+                                                    public void onClick(DialogInterface dialog, int seleccion) {
+                                                        if (opciones[seleccion] == "Elige imagen") {
+                                                            Intent in = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                                                            in.setType("image/*");
+                                                            startActivityForResult(in.createChooser(in, "Selecciona una imagen"), 200);
+                                                        } else if (opciones[seleccion] == "Cancelar") {
+                                                            dialog.dismiss();
+                                                        }
+                                                    }
+                                                });
+                                                builder.show();
+                                                break;
 
+           }
+
+    }
     @Override
     protected void onActivityResult(int requestCode,int resultCode,Intent data){
         super.onActivityResult(requestCode,resultCode,data);
-        if(requestCode == RESULT_OK){
-            Uri path = data.getData();
-            imgview.setImageURI(path);
-        }
+                if(requestCode == RESULT_OK){
+                        Uri path = data.getData();
+                        imgview.setImageURI(path);
+                }
     }
-
-
 }
